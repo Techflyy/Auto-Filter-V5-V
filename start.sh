323c2f1cffa3938a455de0c0db85491fb03edeed
Techflyy <131337420+Techflyy@users.noreply.github.com>
2023-04-22 22:49:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/hemanthbot69/Auto-Filter-V5-V.git /Auto-Filter-V5-V
+  git clone https://github.com/Techflyy/Auto-Filter-V5-V.git /Auto-Filter-V5-V
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Auto-Filter-V5-V
